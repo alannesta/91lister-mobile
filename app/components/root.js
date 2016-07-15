e651937e8520cf52cd692b274fc0ef347036291d
Alan Cao <alannesta@sina.com>
2016-07-15 13:47:52 -0400

    drawerlayout with toolbar

--- app/components/root.js
+++ app/components/root.js
@@ -3,10 +3,16 @@ import {
 	StyleSheet,
 	Text,
 	Navigator,
-	View
+	View,
+	Dimensions,
+	DrawerLayoutAndroid,
+	ToolbarAndroid
 } from 'react-native';
 
 import MovieList from './containers/movie-list';
+
+const DRAWER_WIDTH_LEFT = 56;
+
 class Root extends Component {
 	constructor(props) {
 		super(props);
@@ -15,17 +21,55 @@ class Root extends Component {
 	render() {
 		let movieList = MovieList;
 		return (
-			<Navigator
-				initialRoute={{ name: 'home', component: movieList }}
-				configureScene={(route, routeStack) => Navigator.SceneConfigs.FloatFromLeft}
-				renderScene={(route, navigator) => {
-				   	let Component = route.component;
-				   	return <Component {...route.params} navigator={navigator} />
-			 		}}
-				/>
-
+			<DrawerLayoutAndroid
+        drawerPosition={DrawerLayoutAndroid.positions.Left}
+        drawerWidth={Dimensions.get('window').width - DRAWER_WIDTH_LEFT}
+        keyboardDismissMode="on-drag"
+        ref={(drawer) => { this.drawer = drawer; }}		//! for using this.drawer.open()
+        renderNavigationView={this._renderDrawerContent}
+        statusBarBackgroundColor="#589c90">
+        {this._renderApp()}
+      </DrawerLayoutAndroid>
 		);
 	}
+
+	_renderDrawerContent() {
+		return (
+			<View>
+				<Text>Drawer Content</Text>
+			</View>
+		)
+	}
+
+	_renderApp() {
+		console.log('called');
+		return (
+			<View style={styles.container}>
+				<ToolbarAndroid
+					// logo={require('image!../images/launcher_icon')}
+					// navIcon={require('image!../images/ic_menu_black_24dp')}
+					onIconClicked={() => this.drawer.openDrawer()}
+					style={styles.toolbar}
+					title={'91 lister'}
+				/>
+				<Text>it works!</Text>
+			</View>
+		)
+	}
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  toolbar: {
+    backgroundColor: '#E9EAED',
+    height: 56,
+  },
+  drawerContentWrapper: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
 
 export default Root;
