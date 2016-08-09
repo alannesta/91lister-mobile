026392d2d6b652c27d733dde260f9af749a7bb5a
Alan Cao <alannesta@sina.com>
2016-08-09 11:19:49 -0400

    ios init

---
+++ app/components/root.ios.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {
+	StyleSheet,
+	Text,
+	Navigator,
+	View,
+	Dimensions
+} from 'react-native';
+
+class Root extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+		    <Text>IOS init</Text>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Root;
