c248a984129d38f0080ced65300137f1ec47ebaa
Alan Cao <alannesta@sina.com>
2016-08-20 23:02:05 -0400

    init, having problem with react-native

---
+++ test/actions.spec.js
@@ -0,0 +1,12 @@
+import * as AppActions from '../app/actions/app-actions'
+var expect = require('chai').expect;
+
+describe('app-actions test', () => {
+  it('should update network status', () => {
+    const expectedAction = {
+      type: 'CONNECTION_STATUS_CHANGED',
+      connectionType: 'wifi'
+    }
+    expect(AppActions.updateNetworkStatus('wifi')).to.equal(expectedAction);
+  })
+})
---
+++ test/test.spec.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+
+describe('test ES6 test', function() {
+  it('should work', function() {
+    const kaka = 'kaka';
+    expect(kaka).to.equal('kaka1');
+  })
+})
