91b6147b9f5d5da02a2b35e3c12984a553884eef
Alan Cao <alannesta@sina.com>
2016-08-23 17:03:05 -0400

    movie-list reducer unit test

--- test/movie-list-reducer.spec.js
+++ test/movie-list-reducer.spec.js
@@ -1,30 +1,76 @@
 import reducer from '../app/reducers/movie-list-reducer'
 
+const INITIAL_STATE = {
+  movieData: {
+    movies: [],
+    total: 0
+  },
+  selectedMovieData: {
+    selectedMovie: {
+      id: 0,
+      title: '',
+      url: '',
+      viewCount: 0,
+      trend: 0,
+      liked: false,
+      thumbnail: '',
+      favourite: 0
+    },
+    fileUrl: ''
+  },
+  query: '',
+  mSince: 0,
+  order: 'trend'
+}
+
+const mockedMovies = [...MOCK_MOVIES];
+
 describe('movie-list-reducer', () => {
+
   it('should return the initial state', () => {
     const initialState = reducer(undefined, {});
-    const expectedState = {
-      movieData: {
-    		movies: [],
-    		total: 0
-    	},
-    	selectedMovieData: {
-    		selectedMovie: {
-    			id: 0,
-    			title: '',
-    			url: '',
-    			viewCount: 0,
-    			trend: 0,
-    			liked: false,
-    			thumbnail: '',
-    			favourite: 0
-    		},
-    		fileUrl: ''
-    	},
-    	query: '',
-    	mSince: 0,
-    	order: 'trend'
-    }
-    expect(initialState).to.deep.equal(expectedState);
+    expect(initialState).to.deep.equal(INITIAL_STATE);
   });
+
+  it('should update selectedMovieData state correctly', () => {
+    const updatedState1 = reducer(INITIAL_STATE, {
+      type: 'SELECT_MOVIE',
+      movie: mockedMovies[1],
+    });
+
+    expect(updatedState1).to.deep.equal(Object.assign({}, INITIAL_STATE, {
+      selectedMovieData: {
+        selectedMovie: mockedMovies[1],
+        fileUrl: ''
+      }
+    }));
+
+    const updatedState2 = reducer(updatedState1, {
+      type: 'UPDATE_FILEURL_SUCCESS',
+      fileUrl: 'http://www.mock.com'
+    });
+
+    expect(updatedState2).to.deep.equal(Object.assign({}, updatedState1, {
+      selectedMovieData: {
+        selectedMovie: mockedMovies[1],
+        fileUrl: 'http://www.mock.com'
+      }
+    }));
+  });
+
+  it('should update movieData state correctly', () => {
+    const updatedState = reducer(INITIAL_STATE, {
+      type: 'MOVIE_FETCHED',
+      movies: mockedMovies,
+      total: 3
+    });
+
+    expect(updatedState).to.deep.equal(Object.assign({}, INITIAL_STATE, {
+      movieData: {
+        movies: mockedMovies,
+        total: 3
+      }
+    }));
+  });
+
 })
